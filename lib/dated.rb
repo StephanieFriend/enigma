effbd924434be9d7401d7b4707861c663c122b9b
Stephanie Friend <stephaniefriend91@gmail.com>
2020-03-01 14:42:11 -0700

    Move methods out of key and dated class

--- lib/dated.rb
+++ lib/dated.rb
@@ -1,6 +1,6 @@
-require 'date'
-
-class Dated
+# require 'date'
+#
+# class Dated
 
   # def current_date
   #   DateTime.now.strftime("%d/%m/%y")
@@ -10,14 +10,14 @@ class Dated
   #   gets.chomp
   # end
 
-  def transform_date(date)
-    x = (date.delete("/").to_i ** 2).digits.pop(4)
-    x.reduce({}) do |acc, y|
-      acc[:A] = x[0]
-      acc[:B] = x[1]
-      acc[:C] = x[2]
-      acc[:D] = x[3]
-      acc
-    end
-  end
-end
+#   def transform_date(date)
+#     x = (date.delete("/").to_i ** 2).digits.pop(4)
+#     x.reduce({}) do |acc, y|
+#       acc[:A] = x[0]
+#       acc[:B] = x[1]
+#       acc[:C] = x[2]
+#       acc[:D] = x[3]
+#       acc
+#     end
+#   end
+# end
--- lib/key.rb
+++ lib/key.rb
@@ -1,21 +1,21 @@
-class Key
-
-  def self.generate_random_key
-    rand(10 ** 5).to_s
-  end
-
-  def self.key_input(user_key)
+# class Key
+#
+#   def self.generate_random_key
+#     rand(10 ** 5).to_s
+#   end
+#
+#   def self.key_input(user_key)
     # gets.chomp
-    user_key.to_s.rjust(5, "0")
-  end
-
-  def self.transform_key(key)
-    key.split.reduce({}) do |acc, k|
-      acc[:A] = k[0] + k[1]
-      acc[:B] = k[1] + k[2]
-      acc[:C] = k[2] + k[3]
-      acc[:D] = k[3] + k[4]
-      acc
-    end
-  end
-end
+#     user_key.to_s.rjust(5, "0")
+#   end
+#
+#   def self.transform_key(key)
+#     key.split.reduce({}) do |acc, k|
+#       acc[:A] = k[0] + k[1]
+#       acc[:B] = k[1] + k[2]
+#       acc[:C] = k[2] + k[3]
+#       acc[:D] = k[3] + k[4]
+#       acc
+#     end
+#   end
+# end
