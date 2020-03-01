b81c054278ab9a93735910a6f1ba9bdd8c005ff9
Stephanie Friend <stephaniefriend91@gmail.com>
2020-03-01 14:42:43 -0700

    Add more robust testing to the shift class and update variable naming

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,34 +1,34 @@
 class Shift
 
   def self.transform_key(key)
-    key.split.reduce({}) do |acc, k|
-      acc[:A] = k[0] + k[1]
-      acc[:B] = k[1] + k[2]
-      acc[:C] = k[2] + k[3]
-      acc[:D] = k[3] + k[4]
+    key.split.reduce({}) do |acc, key_index|
+      acc[:A] = key_index[0] + key_index[1]
+      acc[:B] = key_index[1] + key_index[2]
+      acc[:C] = key_index[2] + key_index[3]
+      acc[:D] = key_index[3] + key_index[4]
       acc
     end
   end
 
   def self.transform_date(date)
-    x = (date.delete("/").to_i ** 2).digits.pop(4)
-    x.reduce({}) do |acc, y|
-      acc[:A] = x[0]
-      acc[:B] = x[1]
-      acc[:C] = x[2]
-      acc[:D] = x[3]
+    date_index = (date.delete("/").to_i ** 2).digits.pop(4)
+    date_index.reduce({}) do |acc, _index|
+      acc[:A] = date_index[0]
+      acc[:B] = date_index[1]
+      acc[:C] = date_index[2]
+      acc[:D] = date_index[3]
       acc
     end
   end
 
   def self.final_shift_key(key, date)
-    hash = {}
-    transform_key(key.to_s.rjust(5, "0")).map do |keys, value|
-      total = transform_date(date)[keys]
-      if total != nil
-        hash[keys] = value.to_i + total.to_i
+    final_shifts = {}
+    transform_key(key.to_s.rjust(5, "0")).map do |shift_positions, shift_amount|
+      total_moves = transform_date(date)[shift_positions]
+      if total_moves != nil
+        final_shifts[shift_positions] = shift_amount.to_i + total_moves.to_i
       end
     end
-    hash.values
+    final_shifts.values
   end
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 require './lib/shift'
-require './lib/key'
-require './lib/dated'
+require './lib/enigma'
 
 class ShiftTest < Minitest::Test
 
@@ -11,13 +10,32 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, shift
   end
 
-  def test_it_can_return_final_shift
-    shift = Shift.new
-    dated = Dated.new
+  def test_it_can_transform_key
+    enigma = Enigma.new
 
-    Key.stubs(:generate_random_key).returns("12345")
+    enigma.stubs(:encrypt).returns("12345")
+
+    expected =  {:A=>"12", :B=>"23", :C=>"34", :D=>"45"}
+
+    assert_equal expected, Shift.transform_key("12345")
+  end
+
+  def test_it_can_transform_date
+    enigma = Enigma.new
+
     DateTime.stubs(:now).returns(DateTime.new(2020, 2, 29))
 
-    assert_equal [14, 25, 38, 53], shift.final_shift_key
+    expected = {:A=>2, :B=>2, :C=>4, :D=>8}
+
+    assert_equal expected, Shift.transform_date("290220")
+  end
+
+  def test_it_can_return_final_shift
+    enigma = Enigma.new
+
+    enigma.stubs(:encrypt).returns("12345")
+    DateTime.stubs(:now).returns(DateTime.new(2020, 2, 29))
+
+    assert_equal [14, 25, 38, 53], Shift.final_shift_key("12345", "290220")
   end
 end
